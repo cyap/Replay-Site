326b32b192020d9c6d3fb93f91c1dd156b15e3e2
Chester Yap <chesterdyap@gmail.com>
2016-12-27 21:25:31 -0600

    views

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import index, index2
+from . import index, index2
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
