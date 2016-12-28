73b1a1c931cc8d5ecb35d1eedcde4d659e2e131a
Chester Yap <chesterdyap@gmail.com>
2016-12-28 16:17:19 -0600

    cleaning up directory

--- replaystats-site/wsgi.py
+++ replaystats-site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "replaystats-site.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
