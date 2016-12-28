3aa0718494861e3785a5b6e4cb8b525aa5c57fa3
Chester Yap <chesterdyap@gmail.com>
2016-12-28 16:06:38 -0600

    cleaning up directory

--- replaystats-site/settings.py
+++ replaystats-site/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'staticfiles'), )
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'replaystats-site/static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
