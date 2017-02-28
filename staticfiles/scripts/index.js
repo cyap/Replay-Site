e6e0ee2b7f79209815d6a690a4c6e8065d1daa93
Chester Yap <chesterdyap@gmail.com>
2017-02-28 12:55:34 -0600

    revamping index

--- staticfiles/scripts/index.js
+++ staticfiles/scripts/index.js
@@ -1,36 +1,76 @@
-window.onload = function() {
-	// load clicked tab from localStorage
-	try {
-		document.getElementsByName(localStorage.getItem("openTab"))[0].click();
+
+	/*try {
+	document.getElementById("form_pane").insertAdjacentHTML("afterbegin", localStorage.getItem("filled_form"))
 	}
 	catch(e) {
-		document.getElementsByName("threadTab")[0].click();
-	}
-};
+		console.log("failed");}*/
+$(document).ready(function() {
+		
+	// Attach event handlers to radio buttons
+	$("#parse_types").children().each(function() {
+		$(this).click(function() {
+			$("#parse_pane").children().each(function() {
+				$(this).addClass("hidden");
+				$(":input", this).each(function() {
+					$(this).prop("disabled", true);
+				});
+			});
+			$("#"+this.value).removeClass("hidden")
+			$(":input","#"+this.value).each(function() {
+				$(this).prop("disabled", false);
+			});
+		});
+	});
+	
+	// Add more button
+	$("#stats_more").on("click", function() {
+		//$("#stats_pane").append($("#stats_form").clone());
+		$("#stats_form").clone().insertBefore($("#stats_more"));
+	})
+	
+	// On form input
+	$("#thread_tiers").on("input", function(event) {
+		$("#thread_title").val(event.target.value)
+	});
+	
+	$("#thread_button").click()
+});
+	
+				
+				
+									   
+
+	
+	//document.getElementById("thread_button").click()
+	/*
+	// Function to fill in search
+	Array.prototype.forEach.call(document.getElementsByClassName("tier_input"),
+	function(field) {
+		field.oninput = function() {
+			// If value ends in comma
+			if (field.value.slice(-1) == ",") {
+				// Store as filter
+				// TODO: styling
+				filter = document.createElement("span");
+				filter.textContent = field.value.slice(0,-1)
+				filter.className = "tier_filter"
+				field.parentNode.insertBefore(filter, field)
+				// Insert x: field.parentNode.insertBefore(
+				field.value = ""
+				// TODO: Change to accept hidden input
+			}
+		
+	});*/
 
 window.onbeforeunload = function() {
-	// save clicked tab to localStorage
-	localStorage.setItem("openTab", 
-		document.getElementsByClassName("tabLink active")[0].name);
+	// Save form / option to local storage
+	try {
+		localStorage.setItem("filled_form",
+			document.getElementById("form_pane").innerHTML)
+	}
+	catch(e) {
+		return;
+	}
 }
 
-function openTab(event, tabName) {
-	/* Based on w3schools implementation of tabs */
-	
-	// Get all elements with class="tabcontent" and hide them
-	Array.prototype.forEach.call(document.getElementsByClassName("tabContent"),
-		function(tabContent) {
-			tabContent.style.display = "none";
-		});
-	
-	
-	// Get all elements with class="tablinks" and remove the class "active"
-	Array.prototype.forEach.call(document.getElementsByClassName("tabLink"),
-		function(tabLink) {
-			tabLink.className = tabLink.className.replace(" active", "");
-		});
 
-	// Show the current tab, and add an "active" class to the link that opened the tab
-	document.getElementById(tabName).style.display = "block";
-	event.currentTarget.className += " active";
-}
