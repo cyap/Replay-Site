90138a4a3da5b22668949b8edb320b7fc09c2886
Chester Yap <chesterdyap@gmail.com>
2017-06-03 16:46:49 -0500

    Remove unnecessary staticfile specification

--- rsite/settings.py
+++ rsite/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'rsite/static'), )
+#STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'rsite/static'), )
+STATICFILES_DIRS = ()
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
